7ea632357e82f58ab0cd934c7f280c4470da8acc
Jason DeFontes <jason@defontes.com>
2009-04-23 14:38:21 -0400

    start to unify resource handlers

--- app/resources.py
+++ app/resources.py
@@ -1,4 +1,3 @@
-import cgi
 import logging
 import os
 
@@ -8,67 +7,35 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 
-class ArticleHandler(webapp.RequestHandler):
-  def get(self):  
-    articles = model.Resource.all().filter("path = ", self.request.path).fetch(1)
-    
-    if len(articles) == 1:
-      article = articles[0]
-      
-    template_values = {
-        "resource": article
-      }
+class ResourceHandler(webapp.RequestHandler):
+	def get(self, path):
+		logging.info("Accept: " + self.request.headers['Accept'])
+		resource = model.Resource.all().filter("path = ", self.request.path).get()
+		
+		if resource == None:
+			return not_found(self.response)
+		
+		template_values = {
+			"resource": resource
+		}
+		
+		template_name = resource.template or resource.class_name().lower() + ".html"
+		path = os.path.join(os.path.dirname(__file__), '..', 'templates', template_name )
+		self.response.out.write(template.render(path, template_values))
 
-    template_name = article.template or article.class_name().lower() + ".html"
-    path = os.path.join(os.path.dirname(__file__), '..', 'templates', template_name )
-    self.response.out.write(template.render(path, template_values))
+def not_found(response):
+	path = os.path.join(os.path.dirname(__file__), '..', 'templates', '404.html')
+	response.set_status(404)
+	response.out.write(template.render(path, {} ))
 
-class ResourceHandlerApplication(object):
-  # example of writing our own version of WSGIApplication
-  def __init__(self):
-    self.__debug = True # framework expects this
-
-  def __call__(self, environ, start_response):
-    request = webapp.Request(environ)
-    response = webapp.Response()
-    
-    # TODO - handler mapping goes here
-    handler = ArticleHandler()
-    
-    handler.initialize(request, response)
-    groups = ()
-    if handler:
-      try:
-        method = environ['REQUEST_METHOD']
-        if method == 'GET':
-          handler.get(*groups)
-        elif method == 'POST':
-          handler.post(*groups)
-        elif method == 'HEAD':
-          handler.head(*groups)
-        elif method == 'OPTIONS':
-          handler.options(*groups)
-        elif method == 'PUT':
-          handler.put(*groups)
-        elif method == 'DELETE':
-          handler.delete(*groups)
-        elif method == 'TRACE':
-          handler.trace(*groups)
-        else:
-          handler.error(501)
-      except Exception, e:
-        handler.handle_exception(e, self.__debug)
-    else:
-      response.set_status(404)
-
-    response.wsgi_write(start_response)
-    return ['']
-
-application = ResourceHandlerApplication()
+			
+application = webapp.WSGIApplication( [
+	('(/.*)', ResourceHandler)
+], debug=True)
 
 def main():
-  run_wsgi_app(application)
+	run_wsgi_app(application)
 
 if __name__ == "__main__":
-  main()
+	main()
 
