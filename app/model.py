4167bd6b34cb63d232a98044676c348db58a6906
Jason DeFontes <jason@defontes.com>
2009-04-28 13:26:56 -0400

    refactor some properties out of the base resource class

--- app/model.py
+++ app/model.py
@@ -4,18 +4,19 @@ from google.appengine.ext.db import polymodel
 class Resource(polymodel.PolyModel):
 	parent_resource = db.SelfReferenceProperty(collection_name="child_resources")
 	author = db.UserProperty()
-	body = db.TextProperty()
 	path = db.StringProperty()
-	template = db.StringProperty()
 	title = db.StringProperty()
 	creation_date = db.DateTimeProperty(auto_now_add=True)
 	modification_date = db.DateTimeProperty(auto_now=True)
 
 class Folder(Resource):
+	body = db.TextProperty()
 	browse = db.BooleanProperty()
+	template = db.StringProperty()
 
 class Article(Resource):
-	pass
+	body = db.TextProperty()
+	template = db.StringProperty()
 
 class Image(Resource):
 	image_blob = db.BlobProperty()
