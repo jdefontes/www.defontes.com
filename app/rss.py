86592a823775df85eb45e8655abf5609f1b7b75c
Jason DeFontes <jason@defontes.com>
2009-05-09 23:11:15 -0400

    use date formatting from email package

--- app/rss.py
+++ app/rss.py
@@ -1,4 +1,4 @@
-﻿from datetime import datetime
+﻿from email.utils import formatdate
 from xml.dom import getDOMImplementation
 
 class RssFeed:
@@ -29,7 +29,7 @@ class RssFeed:
         channel.appendChild(elem)
         
         elem = self.document.createElement("lastBuildDate")
-        elem.appendChild(self.document.createTextNode(rfc822_date(datetime.now())))
+        elem.appendChild(self.document.createTextNode(formatdate()))
         channel.appendChild(elem)
         
         if email is not None:
@@ -44,22 +44,3 @@ class RssFeed:
 
     def to_xml(self):
         return self.document.toprettyxml()
-        
-def rfc822_date(dt):
-    """convert a datetime into an RFC 822 formatted date
-
-    Input date must be in GMT.
-    """
-    # Looks like:
-    #   Sat, 07 Sep 2002 00:00:01 GMT
-    # Can't use strftime because that's locale dependent
-    #
-    # Isn't there a standard way to do this for Python?  The
-    # rfc822 and email.Utils modules assume a timestamp.  The
-    # following is based on the rfc822 module.
-    return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (
-            ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt.weekday()],
-            dt.day,
-            ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-             "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][dt.month-1],
-            dt.year, dt.hour, dt.minute, dt.second)
